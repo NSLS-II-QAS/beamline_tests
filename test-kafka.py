045816412782f37ef7ee53ab65f362c8bcbec022
Julien Lhermitte <lhermitte@bnl.gov>
2018-04-23 16:44:53 -0400

    working test

--- test-kafka.py
+++ test-kafka.py
@@ -7,7 +7,7 @@ from bluesky.run_engine import Dispatcher
 from bluesky.callbacks.core import CallbackBase
 
 
-class Producer(CallbackBase):
+class Publisher(CallbackBase):
     """
     A callback that publishes documents to kafka.
     Parameters
@@ -32,27 +32,32 @@ class Consumer(Dispatcher):
     '''
     '''
     def __init__(self, bootstrap_servers, topic):
+        self._task = None
+        self.loop = asyncio.get_event_loop()
         self._topic = topic
-        self._consumer = KafkaConsumer(topic, booststrap_servers=bootstrap_servers)
+        self._consumer = KafkaConsumer(topic, bootstrap_servers=bootstrap_servers)
 
     @asyncio.coroutine
     def _poll(self):
         while True:
-            message = yield from self._socket.recv()
-            hostname, pid, RE_id, name, doc = message.split(b' ', 4)
-            hostname = hostname.decode()
-            pid = int(pid)
-            RE_id = int(RE_id)
-            name = name.decode()
-            if self._is_our_message(hostname, pid, RE_id):
-                doc = pickle.loads(doc)
-                self.loop.call_soon(self.process, DocumentNames[name], doc)
+            result = yield next(self._consumer)
+            bs = result.value
+            print("got message: {}".format(bs))
+            #hostname, pid, RE_id, name, doc = message.split(b' ', 4)
+            #hostname = hostname.decode()
+            #pid = int(pid)
+            #RE_id = int(RE_id)
+            #name = name.decode()
+            #if self._is_our_message(hostname, pid, RE_id):
+                #doc = pickle.loads(doc)
+                #self.loop.call_soon(self.process, DocumentNames[name], doc)
 
     def start(self):
         try:
             self._task = self.loop.create_task(self._poll())
             self.loop.run_forever()
         except:
+            print("Error, stopping")
             self.stop()
             raise
 
@@ -63,9 +68,21 @@ class Consumer(Dispatcher):
         self._task = None
 
 
+''' Callback method (to debug)
+bootstrap_servers = ['cmb01:9092', 'cmb02:9092']
+topic = 'qas-analysis'
 
-topic = 'databroker1'
-msg = bytes(b"test message")
+prod = Publisher(bootstrap_servers=bootstrap_servers, topic=topic)
+consumer = Consumer(bootstrap_servers=bootstrap_servers, topic=topic)
+
+prod.start(dict(name="julien", msg="test"))
+
+print("starting consumer")
+consumer.start()
+print("done")
+'''
+
+topic = 'qas-analysis'
 send_doc = dict(name='julien', subject="test")
 
 print("Sending {}".format(send_doc))
@@ -73,13 +90,20 @@ print("Sending {}".format(send_doc))
 bs = json.dumps(send_doc).encode('utf-8')
 
 bootstrap_servers = ['cmb01:9092', 'cmb02:9092']
-publisher = KafkaProducer(bootstrap_servers=bootstrap_servers)
-publisher.send(topic, bs)
-
 consumer = KafkaConsumer(topic, bootstrap_servers=bootstrap_servers)
-#bs = consumer.recv()
-#recv_doc = json.loads(bs)
-for bs in consumer:
-    print("received {}".format(json.loads(bs)))
+
+def receive():
+    print("now receiving...")
+    for bs in consumer:
+        print("received {}".format(json.loads(bs.value)))
 
 
+from threading import Thread
+th = Thread(target=receive)
+th.daemon=True
+
+th.start()
+
+publisher = KafkaProducer(bootstrap_servers=bootstrap_servers)
+result = publisher.send(topic, bs)
+print(result.get())
